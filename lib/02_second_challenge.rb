eb3da18a4ef873eb1e9b6a40ebf1bb2be2a0a989
Matthew Aquino <matthewaquino96@gmail.com>
2020-12-31 02:24:04 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,19 +1,16 @@
 def second_challenge
-  epic_tragedy = {
-    :montague => {
-      #your key/value pairs here!
+epic_tragedy = {
+   :montague => {
       :patriarch => {},
       :matriarch => {},
       :hero => {},
       :hero_friends => []
-    }, 
-    :capulet => {
-      #your key/value pairs here!
+   },
+   :capulet => {
       :patriarch => {},
       :matriarch => {},
-      :hero => {},
-      :hero_friends => []
-    }
-    
-  }
+      :heroine => {},
+      :heroine_friends => []
+   }
+}
 end
